fc529ed40526bbd626cb388b17b784c6d51dd378
zeinima13 <136815397@qq.com>
2025-01-20 10:41:04 +0800

    Update App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,31 @@
 <template>
-  <el-config-provider :locale="zhCn">
+  <div id="app">
     <router-view></router-view>
-  </el-config-provider>
+  </div>
 </template>
 
 <script>
-import { ElConfigProvider } from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-
 export default {
-  name: 'App',
-  components: {
-    ElConfigProvider
-  },
-  setup() {
-    return {
-      zhCn
-    }
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
+  min-height: 100vh;
+}
+
+body {
   margin: 0;
   padding: 0;
 }
 
 * {
   box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
 }
 </style>
