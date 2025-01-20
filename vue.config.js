6d3fdec00a161edd9fb63c3b81b1434e098a92fc
zeinima13 <136815397@qq.com>
2025-01-20 10:50:30 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/account-shop/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/account-shop/'
+    : '/',
   outputDir: 'docs'
 })
