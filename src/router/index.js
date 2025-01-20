1c5bfb9b8096faea1c35d191207835dee99df8e6
zeinima13 <136815397@qq.com>
2025-01-20 16:48:36 +0800

    Update path configuration for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory('./'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
