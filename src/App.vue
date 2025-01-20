8570f03e640b23e82ad458be4541358535198dab
zeinima13 <136815397@qq.com>
2025-01-20 11:08:38 +0800

    Fix routing and app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <el-config-provider :locale="zhCn">
-    <div id="app">
-      <router-view></router-view>
-    </div>
-  </el-config-provider>
+  <router-view />
 </template>
 
 <script>
@@ -25,20 +21,19 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  min-height: 100vh;
-}
-
-body {
   margin: 0;
   padding: 0;
 }
 
 * {
   box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
